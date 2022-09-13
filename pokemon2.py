d0aa15240f47e9a29752a1e6167a46207213eee5
GabrielMejia03 <112830347+GabrielMejia03@users.noreply.github.com>
2022-09-12 23:54:22 -0500

    Update pokemon2.py

--- pokemon2.py
+++ pokemon2.py
@@ -1,3 +1,8 @@
+# Script en python que consulta el api de pokemon
+# para listar los nombres de pokemon pero se le agrego
+# interacción para que listaras más pokemons segun se vaya requiriendo.
+# Contribuyo: <Aquí va tu nombre>
+# Fecha: < Aquí va la fecha en que modificas este archivo>
 import requests
 #
 def get_pokemons (url='http://pokeapi.co/api/v2/pokemon-form/', offset=0):
